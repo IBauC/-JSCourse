320517345b2ee7248f5507e29dc7c397f243ab1a
Ivan <158612285+IBauC@users.noreply.github.com>
2024-02-12 23:45:37 +0200

    Update classwork3.js

--- classwork3.js
+++ classwork3.js
@@ -10,6 +10,48 @@ function getTimeOfDay(currentHour) {
     }
 }
 
+
 const currentHour = (new Date()).getHours(); 
 const timeOfDay = getTimeOfDay(currentHour);
 console.log("Сейчас " + timeOfDay);
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+
+for (let i = 1; i <= 10; i++) {
+    if (i > 7) {
+        break; 
+    }
+    console.log(i);
+}
+
+
+    let i = 1; 
+
+while (i <= 20) {
+    if (i % 2 === 0 && i % 5 !== 0) {
+        console.log(i);
+    }
+    i++; 
+}
+
+
+function createPhoneNumber(numbers){
+  if(numbers.length === 10){
+    let result = "(";
+    for (let i = 0; i <= 2; ++i)
+      result += numbers[i].toString();
+    result += ") ";
+    for (let i = 3; i <= 5; ++i){
+      result += numbers[i].toString();
+    }
+    result += "-";
+    for (let i = 6; i < numbers.length; ++i) 
+      result += numbers[i].toString();
+    return result;
+  } else {
+    return "Неверное количество цифр в номере.";
+  }
+}
+const phoneNumber = createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+console.log(phoneNumber);
